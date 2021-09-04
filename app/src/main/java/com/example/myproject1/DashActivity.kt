c2454075ead9d06554fc3a5ccdde28060eedd081
mcapcha0108 <89178090+mcapcha0108@users.noreply.github.com>
2021-09-03 21:06:25 -0500

    encuesta_p1

--- app/src/main/java/com/example/myproject1/DashActivity.kt
+++ app/src/main/java/com/example/myproject1/DashActivity.kt
@@ -1,15 +1,27 @@
 package com.example.myproject1
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_dash.*
+import kotlinx.android.synthetic.main.activity_datos_clientes.*
+import android.R.id
+
+
+
 
 class DashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash)
+
+        linearLayout_2.setOnClickListener {
+            startActivity(Intent(this, DatosClientesActivity::class.java))
+        }
+
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/myproject1/ServiciosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServiciosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_servicios)
+    }
+}
