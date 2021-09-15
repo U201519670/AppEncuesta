08b62ddeecb3dbaa24a145787bb564d3bce05b36
Marko Juli <u202118554@upc.edu.pe>
2021-09-15 16:04:18 -0500

    marko_1509_2

---
+++ app/src/main/java/com/example/myproject1/RegaloUser.kt
@@ -0,0 +1,11 @@
+package com.example.myproject1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegaloUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regalo_user)
+    }
+}
