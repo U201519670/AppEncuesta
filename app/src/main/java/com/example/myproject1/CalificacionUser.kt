86a68ae04f92447d46680b75fc9f3c8391a5456a
Marko Juli <u202118554@upc.edu.pe>
2021-09-15 19:18:02 -0500

    marko_1509_4

--- app/src/main/java/com/example/myproject1/CalificacionUser.kt
+++ app/src/main/java/com/example/myproject1/CalificacionUser.kt
@@ -1,11 +1,22 @@
 package com.example.myproject1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calificacion_user.*
 
 class CalificacionUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calificacion_user)
+
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, f1 , b->
+            textrating.text="rating:"+f1.toString()
+        }
+
+        btn_calificar.setOnClickListener {
+            startActivity(Intent(this, GoodbyeActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/myproject1/RegaloUser.kt
+++ app/src/main/java/com/example/myproject1/RegaloUser.kt
@@ -1,11 +1,42 @@
 package com.example.myproject1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_regalo_user.*
+import java.util.*
+
+
 
 class RegaloUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regalo_user)
+
+        var numero= (1..60).random()
+        Log.i("-->", numero.toString())
+        var premio=""
+        if(numero>=1&&numero<10) {
+            premio = "ACG60812-SD"
+        }else if(numero>=10 &&numero<20) {
+            premio = "SIGUE INTENTANDO"
+        }else if(numero>=20 &&numero<30) {
+            premio = "A65D21F2-22"
+        }else if(numero>30 &&numero<40) {
+            premio = "SIGUE INTENTANDO"
+        }else if(numero>40 &&numero<50) {
+            premio = "GSFG2DFS-50"
+        }else{
+            premio = "SIGUE INTENTANDO"
+        }
+
+        btn_abrir.setOnClickListener {
+            mostrarpremio.text=premio
+        }
+
+        btnsalir.setOnClickListener {
+            startActivity(Intent(this, GoodbyeActivity::class.java))
+        }
     }
 }
